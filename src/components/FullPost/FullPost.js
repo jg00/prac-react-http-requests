6ff0bb0298fc6de2796446c9131f44b0327e9df6
Jack Gamboa <jgamboa929@gmail.com>
2019-05-04 18:52:42 -0500

    FullPost.js - http get post by id on componentDidUpdate

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
+import axios from "axios";
 import classes from "./FullPost.module.css";
 
 export class FullPost extends Component {
+  state = {
+    loadedPost: null
+  };
+
+  // Capture post by id
+  componentDidUpdate() {
+    // Send http request only if we want to load a new post
+    if (this.props.id) {
+      /* 1 If we initially do not have a loadedPost ie null
+         2 or If we do have a loadedPost and current loadedPost.id not equal to new props.id (ie different)*/
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios
+          .get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+          .then(response => {
+            this.setState({
+              loadedPost: response.data
+            });
+          });
+      }
+    }
+  }
+
   render() {
-    let post = <p>Please select a post</p>;
-    post = (
-      <div className={classes.FullPost}>
-        <h1>Title</h1>
-        <p>Content</p>
-        <div className={classes.Edit}>
-          <button className={classes.Delete}>Delete</button>
+    // Initial display
+    let post = <p style={{ textAlign: "center" }}>Please select a post</p>;
+
+    // Temporary display until data fetched
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading..</p>;
+    }
+
+    // Defer render till we get loadedPost properties
+    if (this.state.loadedPost) {
+      post = (
+        <div className={classes.FullPost}>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className={classes.Edit}>
+            <button className={classes.Delete}>Delete</button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
     return post;
   }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,21 +7,41 @@ import NewPost from "../../components/NewPost/NewPost";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      // Limit posts; Add "author" property placeholder b/c jsonplaceholder does not include author data.
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return { ...post, author: "Malone" };
+      });
+
       this.setState({
-        posts: response.data
+        posts: updatedPosts
       });
     });
   }
 
+  postSelectedHandler = id => {
+    this.setState({
+      selectedPostId: id
+    });
+  };
+
   render() {
     // Posts
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
@@ -29,11 +49,9 @@ class Blog extends Component {
         <header>
           <h1>Blogging</h1>
         </header>
-        <section className={classes.Posts}>
-          {posts}>{posts}
-        </section>
+        <section className={classes.Posts}>{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
